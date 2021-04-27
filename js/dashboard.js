86a0ce28e50df91b91c70fa30c07fe624cc378fb
scasaf00 <scasaf00@estudiantes.unileon.es>
2021-04-27 02:29:42 +0200

    avanzando bestia, casi full frontend y casi backend

--- js/dashboard.js
+++ js/dashboard.js
@@ -7,6 +7,7 @@ const switchLabel = document.querySelector(".switch label");
 const switchLabelText = switchLabel.querySelector("span:last-child");
 const collapsedClass = "collapsed";
 const lightModeClass = "light-mode";
+const clueTimes = 3;
 
 /*TOGGLE HEADER STATE*/
 collapseBtn.addEventListener("click", function () {
@@ -51,3 +52,84 @@ switchInput.addEventListener("input", function () {
         localStorage.setItem("dark-mode", "true");
     }
 });
+
+/* PRACTICE SCRIPT */
+let http_request = false;
+let dictionary = null;
+let temp_clues = clueTimes;
+
+function readDictionary(){
+    http_request = false;
+    if (window.XMLHttpRequest) {
+        http_request = new XMLHttpRequest();
+        if (http_request.overrideMimeType) {
+            http_request.overrideMimeType('text/xml');
+        }
+    } else if (window.ActiveXObject) { // IE
+        try {
+            http_request = new ActiveXObject("Msxml2.XMLHTTP");
+        } catch (e) {
+            try {
+                http_request = new ActiveXObject("Microsoft.XMLHTTP");
+            } catch (e) {}
+        }
+    }
+    if (!http_request) {
+        alert('Falla :( No es posible crear una instancia XMLHTTP');
+        return false;
+    }
+    http_request.onreadystatechange = loadContent;
+    http_request.open('GET', 'https://ordenalfabetix.unileon.es/aw/diccionario.txt', true);
+    http_request.send();
+}
+
+function loadContent() {
+
+    if (http_request.readyState === 4) {
+        if (http_request.status === 200) {
+            dictionary = http_request.responseText;
+        } else {
+            alert('Hubo problemas con la petición.');
+        }
+    }
+
+    document.getElementById('counter').innerHTML = temp_clues;
+}
+
+function searchClue(){
+
+    let searchTerm = document.getElementById('searchterm').value.toLowerCase();
+    let out = [];
+    let flag = [];
+
+    searchTerm = Array.from(searchTerm);
+
+    if(searchTerm.length === 0){
+        document.getElementById('cluesArea').innerHTML = 'Cadena de busqueda erronea';
+        return null;
+    }
+
+    if(temp_clues === 0) {
+        alert('No te quedan pistas :(');
+        document.getElementById('cluesArea').innerHTML = "";
+        return null;
+    }
+
+
+    const clues = dictionary.split('\n');
+    for(let i = 0; i < clues.length; i++){
+        flag.splice(0, flag.length);
+        for(let j = 0; j < searchTerm.length; j++){
+            if(!clues[i].includes(searchTerm[j]))
+                flag.push(1);
+        }
+
+        if(flag.length === 0)
+            out.push(clues[i]);
+    }
+
+    document.getElementById('cluesArea').innerHTML = out.join('\n');
+    temp_clues -= 1;
+    document.getElementById('counter').innerHTML = temp_clues;
+}
+
