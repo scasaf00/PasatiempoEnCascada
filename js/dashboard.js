03fca105aad03aeb279f79f3e4c7960ade05907f
scasaf00 <scasaf00@estudiantes.unileon.es>
2021-04-27 13:54:27 +0200

    fin del frontend? casi todo backend

--- js/dashboard.js
+++ js/dashboard.js
@@ -8,6 +8,7 @@ const switchLabelText = switchLabel.querySelector("span:last-child");
 const collapsedClass = "collapsed";
 const lightModeClass = "light-mode";
 const clueTimes = 3;
+const table = document.getElementById('pastime');
 
 /*TOGGLE HEADER STATE*/
 collapseBtn.addEventListener("click", function () {
@@ -132,4 +133,132 @@ function searchClue(){
     temp_clues -= 1;
     document.getElementById('counter').innerHTML = temp_clues;
 }
+
+function readTable(){
+
+    const tableRows = table.rows;
+    console.log(tableRows);
+    for(let i = 0; i < tableRows.length; i++){
+        for(let j = 0; j < tableRows[i].children.length -1 ; j++){
+            console.log(tableRows[i].children[j].children[0].value);
+        }
+    }
+}
+
+function checkWord(row){
+    let word = [];
+    for(let i = 0; i < table.rows[row].children.length -1; i++){
+        word.push(table.rows[row].children[i].children[0].value);
+    }
+    word = word.join('').toLowerCase();
+
+    switch (row) {
+        case 0:
+            if(word === 'clan'){
+                for(let i = 0; i < table.rows[row].children.length - 1; i++){
+                    table.rows[row].children[i].children[0].style.backgroundColor = '#c3f6c3';
+                }
+            }else{
+                for(let i = 0; i < table.rows[row].children.length - 1; i++) {
+                    table.rows[row].children[i].children[0].style.backgroundColor = '#f5b3be';
+                }
+            }
+            break;
+        case 5:
+            if(word === 'pena'){
+                for(let i = 0; i < table.rows[row].children.length - 1; i++){
+                    table.rows[row].children[i].children[0].style.backgroundColor = '#c3f6c3';
+                }
+            }else{
+                for(let i = 0; i < table.rows[row].children.length - 1; i++) {
+                    table.rows[row].children[i].children[0].style.backgroundColor = '#f5b3be';
+                }
+            }
+            break;
+        case 6:
+            if(word === 'remato'){
+                for(let i = 0; i < table.rows[row].children.length - 1; i++){
+                    table.rows[row].children[i].children[0].style.backgroundColor = '#c3f6c3';
+                }
+            }else{
+                for(let i = 0; i < table.rows[row].children.length - 1; i++) {
+                    table.rows[row].children[i].children[0].style.backgroundColor = '#f5b3be';
+                }
+            }
+            break;
+        case 11:
+            if(word === 'torero'){
+                for(let i = 0; i < table.rows[row].children.length - 1; i++){
+                    table.rows[row].children[i].children[0].style.backgroundColor = '#c3f6c3';
+                }
+            }else{
+                for(let i = 0; i < table.rows[row].children.length - 1; i++) {
+                    table.rows[row].children[i].children[0].style.backgroundColor = '#f5b3be';
+                }
+            }
+            break;
+        default:
+            if(checkDictionary(word) && checkPrevious(word, row)){
+                for(let i = 0; i < table.rows[row].children.length - 1; i++){
+                    table.rows[row].children[i].children[0].style.backgroundColor = '#c3f6c3';
+                }
+            }else{
+                for(let i = 0; i < table.rows[row].children.length - 1; i++) {
+                    table.rows[row].children[i].children[0].style.backgroundColor = '#f5b3be';
+                }
+            }
+    }
+}
+
+function checkDictionary(word) {
+    let words = dictionary.split('\n');
+    for(let i = 0; i < words.length; i++){
+        if(word === words[i])
+            return true;
+    }
+    return false;
+}
+
+function checkPrevious(word, row){
+//TODO
+}
+
+function nextCell(row, pos){
+    if (event.keyCode !== 8) {
+        switch (row) {
+            case 0: //Next
+            case 1: //Next
+            case 2: //Next
+            case 3: //Next
+            case 4: //Next
+            case 5:
+                if (pos !== 3) {
+                    table.rows[row].children[pos + 1].children[0].focus();
+                } else if (pos === 3) {
+                    table.rows[row + 1].children[0].children[0].focus();
+                }
+                break;
+            case 6: //Next
+            case 7: //Next
+            case 8: //Next
+            case 9: //Next
+            case 10:
+                if (pos !== 5) {
+                    table.rows[row].children[pos + 1].children[0].focus();
+                } else if (pos === 5) {
+                    table.rows[row + 1].children[0].children[0].focus();
+                }
+                break;
+            case 11:
+                if (pos !== 5) {
+                    table.rows[row].children[pos + 1].children[0].focus();
+                } else if (pos === 5) {
+                    table.rows[0].children[0].children[0].focus();
+                }
+                break;
+        }
+    }
+}
+
+
 
